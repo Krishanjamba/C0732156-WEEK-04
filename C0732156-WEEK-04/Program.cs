8a6f5290d009958ec41a5deca01c05417af815ca
visualstudio <visualstudio@122-PC22>
2019-01-30 11:23:08 -0500

    Change1

--- C0732156-WEEK-04/Program.cs
+++ C0732156-WEEK-04/Program.cs
@@ -20,9 +20,34 @@ namespace C0732156_WEEK_04
     {
         /* Node is an ADT */
         // What kind of DATA do we need in a NODE? 
-        public village nextvillage;
-        public String villageName;
+        public village nextVillage;
+        public village previousvillage;
+        public String VillageName;
         public bool isAstrildeHere = false;
+    }
+    class Countryside
+    {
+        village Maple = new village();
+       
+        village Toronto = new village();
+        
+        village Ajax = new village();
+        village First;
+      
+        public void Lunch()
+        {
+           
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousvillage = null;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousvillage = Maple;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousvillage = Toronto;
 
+
+        }
     }
 }
