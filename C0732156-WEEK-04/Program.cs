e89676e5dbba8abd9dd89f2bb56c81631576e7d0
visualstudio <visualstudio@122-PC22>
2019-01-30 10:22:19 -0500

    commit

--- C0732156-WEEK-04/Program.cs
+++ C0732156-WEEK-04/Program.cs
@@ -7,9 +7,22 @@ using System.Threading.Tasks;
 namespace C0732156_WEEK_04
 {
     class Program
+
+
     {
         static void Main(string[] args)
         {
+
         }
+    }
+
+    class village
+    {
+        /* Node is an ADT */
+        // What kind of DATA do we need in a NODE? 
+        public village nextvillage;
+        public String villageName;
+        public bool isAstrildeHere = false;
+
     }
 }
